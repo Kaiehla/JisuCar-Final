e77336a5a07a63ca27857b1a24e9e30325c901c5
kaiehla <espiritu.kaiehla@gmail.com>
2024-05-09 15:44:37 +0800

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import { getServerSession } from "next-auth";
 import SessionProvider from "@/components/auth/SessionProvider";
@@ -6,19 +6,55 @@ import { Next13NProgress, Link } from "nextjs13-progress";
 // Bootstrap
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 // Google font - Poppins
 import { Poppins } from "next/font/google";
 const poppins = Poppins({
-   subsets: ['latin'],
-   display: 'swap',
-   variable: '--font-poppins',
-   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
- });
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+//  PWA
+const APP_NAME = "JisuCar";
+const APP_DEFAULT_TITLE = "JisuCar";
+const APP_TITLE_TEMPLATE = "JisuCar";
+const APP_DESCRIPTION = "JisuCar App";
 
 export const metadata: Metadata = {
-  title: "JisuCar",
-  description: "JisuCar WEBSITE",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
   icons: [
     {
       rel: "icon",
@@ -28,10 +64,17 @@ export const metadata: Metadata = {
   ],
 };
 
+export const viewport: Viewport = {
+  themeColor: "#000",
+};
+
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const session = await getServerSession();
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="manifest.json" />
+      </head>
       <body className={poppins.className}>
         <SessionProvider session={session}>
           <main>{children}</main>
--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} = require("next/constants");
+
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -22,6 +27,15 @@ const nextConfig = {
       },
     ],
   },
+  reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withPWA = require("@ducanh2912/next-pwa").default({
+      dest: "public",
+    });
+    return withPWA(nextConfig);
+  }
+  return nextConfig;
+};
